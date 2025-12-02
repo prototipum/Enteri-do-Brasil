62b7e1612efa2af8e7e778962c823920fecf217e
Ygor Scofano <cinzeiro@outlook.com.br>
2025-12-02 13:48:25 -0300

    Loading screen

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
+const hide = document.querySelector('.hide');
+const loader = document.querySelector('.loader');
+window.addEventListener('load', () => {
+    loader.style.display = 'none';
+    hide.style.display = 'block';
+});
 function showHeaderOnTop() {
     const header = document.querySelector('.header');
     document.addEventListener('scroll', () => {
